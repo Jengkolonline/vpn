01966c82beb2d7d3c261fd940b2a6d9dd87b4878
Jengkolonline <134737979+Jengkolonline@users.noreply.github.com>
2023-06-27 20:10:21 +0700

    Update ssh-vpn.sh

--- ssh-vpn.sh
+++ ssh-vpn.sh
@@ -155,6 +155,7 @@ rm /etc/nginx/sites-enabled/default
 rm /etc/nginx/sites-available/default
 wget -O /etc/nginx/nginx.conf "https://raw.githubusercontent.com/Jengkolonline/tunneljengkolonline/main/ssh/nginx.conf"
 mkdir -p /home/vps/public_html
+wget -O /etc/nginx/conf.d/vps.conf "https://raw.githubusercontent.com/Jengkolonline/vpn/main/vps.conf"
 /etc/init.d/nginx restart
 
 # install badvpn
