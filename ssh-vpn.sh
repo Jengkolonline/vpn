083199ad390f8c6975dca27a2857119d8bcc4b46
Jengkolonline <134737979+Jengkolonline@users.noreply.github.com>
2023-08-22 14:55:55 +0700

    Update ssh-vpn.sh

--- ssh-vpn.sh
+++ ssh-vpn.sh
@@ -466,28 +466,31 @@ apt autoremove -y >/dev/null 2>&1
 cd
 chown -R www-data:www-data /home/vps/public_html
 sleep 1
-echo -e "$yell[SERVICE]$NC Restart All service SSH & OVPN"
+echo -e "$yell[SERVICE]$NC Restart Nginix"
 /etc/init.d/nginx restart >/dev/null 2>&1
 sleep 1
-echo -e "[ ${green}ok${NC} ] Restarting nginx"
+echo -e "[ ${green}ok${NC} ] Restart All service SSH & OVPN"
 /etc/init.d/openvpn restart >/dev/null 2>&1
 sleep 1
-echo -e "[ ${green}ok${NC} ] Restarting cron "
+echo -e "[ ${green}ok${NC} ] Restarting ssh "
 /etc/init.d/ssh restart >/dev/null 2>&1
 sleep 1
-echo -e "[ ${green}ok${NC} ] Restarting ssh "
+echo -e "[ ${green}ok${NC} ] Restarting dropbear "
 /etc/init.d/dropbear restart >/dev/null 2>&1
 sleep 1
-echo -e "[ ${green}ok${NC} ] Restarting dropbear "
+echo -e "[ ${green}ok${NC} ] Restarting fail2ban "
 /etc/init.d/fail2ban restart >/dev/null 2>&1
 sleep 1
-echo -e "[ ${green}ok${NC} ] Restarting fail2ban "
-/etc/init.d/stunnel4 restart >/dev/null 2>&1
+echo -e "[ ${green}ok${NC} ] Restarting SSLH "
+/etc/init.d/fail2ban restart >/dev/null 2>&1
 sleep 1
-echo -e "[ ${green}ok${NC} ] Restarting stunnel4 "
-/etc/init.d/vnstat restart >/dev/null 2>&1
+echo -e "[ ${green}ok${NC} ] Restarting stunnel5 "
+/etc/init.d/stunnel5 restart >/dev/null 2>&1
 sleep 1
 echo -e "[ ${green}ok${NC} ] Restarting vnstat "
+/etc/init.d/vnstat restart >/dev/null 2>&1
+sleep 1
+echo -e "[ ${green}ok${NC} ] Restarting squid "
 /etc/init.d/squid restart >/dev/null 2>&1
 screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7100 --max-clients 500
 screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7200 --max-clients 500
